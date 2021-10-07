56632f03ec95c2a7d85a04eb1b9ff056d2ace595
ckdals82 <81973067+ckdals82@users.noreply.github.com>
2021-10-07 20:44:02 +0900

    contact me
    contact me 누르면 하단으로 이동하게끔

--- main.js
+++ main.js
@@ -16,13 +16,23 @@ document.addEventListener('scroll', () => {
 //navbar 클릭스 이동 구현
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-  console.log(EventTarget);
+  //   console.log(EventTarget);
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  //   console.log(event.target.dataset.link);
+  scrollIntoView(link);
 });
+
+//contact me 눌렀을때 이동
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
