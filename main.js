14d775efb613f66bb328190a80967f2407b368d8
ckdals82 <81973067+ckdals82@users.noreply.github.com>
2021-10-07 17:37:40 +0900

    navbar menu click
    navbar 메뉴 클릭시 해당 메뉴로 이동 구현

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 'use strict';
+
+//navbar 투명하게 만들기 스크롤시
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -9,4 +11,18 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+//navbar 클릭스 이동 구현
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  console.log(EventTarget);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
